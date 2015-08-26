eb45570f370d2b50795e7cf5953014a06c8fbd5e
Ian McKenney & Kevin Tokheim <student@epicodus.com>
2015-08-26 10:29:12 -0700

    add Author class and tests.

--- src/Author.php
+++ src/Author.php
@@ -3,5 +3,50 @@
     {
         private $name;
         private $id;
+
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO authors (name) VALUES ('{$this->getName()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_authors = $GLOBALS['DB']->query("SELECT * FROM authors;");
+            $authors = array();
+            foreach ($returned_authors as $author) {
+                $name = $author['name'];
+                $id = $author['id'];
+                $new_author = new Author($name, $id);
+                array_push($authors, $new_author);
+            }
+            return $authors;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM authors;");
+        }
     }
 ?>
---
+++ tests/AuthorTest.php
@@ -0,0 +1,119 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Book.php";
+    require_once "src/Author.php";
+
+    $server = 'mysql:host=localhost;dbname=library_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class AuthorTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Book::deleteAll();
+            Author::deleteAll();
+        }
+
+        function test_setName()
+        {
+            //Arrange
+            $name = "Kevin";
+            $test_author = new Author($name);
+
+            //Act
+            $name2 = "Ian";
+            $test_author->setName($name2);
+            $result = $test_author->getName();
+
+            //Assert
+            $this->assertEquals($name2, $result);
+        }
+
+        function test_getName()
+        {
+            //Arrange
+            $name = "Mark Twain";
+            $test_author = new Author($name);
+
+            //Act
+            $result = $test_author->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+        function test_getId()
+        {
+            //Arrange
+            $name = "Andy Weir";
+            $id = 1;
+            $test_author = new Author($name, $id);
+
+            //Act
+            $result = $test_author->getId();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $name = "Roberto Bolano";
+            $test_author = new Author($name);
+            $test_author->save();
+
+            //Act
+            $result = Author::getAll();
+
+            //Assert
+            $this->assertEquals($test_author, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "David Foster Wallace";
+            $test_author = new Author($name);
+            $test_author->save();
+
+            $name2 = "Stephen King";
+            $test_author2 = new Author($name);
+            $test_author2->save();
+
+            //Act
+            $result = Author::getAll();
+
+            //Assert
+            $this->assertEquals([$test_author, $test_author2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $name = "David Foster Wallace";
+            $test_author = new Author($name);
+            $test_author->save();
+
+            $name2 = "Stephen King";
+            $test_author2 = new Author($name2);
+            $test_author2->save();
+
+            //Act
+            Author::deleteAll();
+            $result = Author::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+    }
+
+
+?>
